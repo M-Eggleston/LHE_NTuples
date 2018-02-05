e4c659a80fe8761fa54ff6b0abd454dfd8a6864e
Michael Eggleston <Eggleston.Mike@gmail.com>
2018-02-05 12:26:23 -0500

    Added data extraction to parser, and tree filling

--- LHEConverter.py
+++ LHEConverter.py
@@ -40,9 +40,9 @@ def getMetaInfo(line):
         alpha_s = data[5]
     else:
         print '{} data points were found, when 6 were expected! Skipping to next event.'.format(len(data))
+    #TODO: actually force the parser to skip to next event
 
 def main():
-    global num_particles
     args = parser.parse_args()
     #Check for an input file
     if len(sys.argv) == 0 or not args.in_file:
@@ -53,6 +53,12 @@ def main():
     if not args.out_name:
         out_f_name = args.in_file.split('.lhe')[0] + '.root'
 
+    global num_particles
+    global event_weight
+    global event_scale
+    global alpha_em
+    global alpha_s
+
     #---------------------------------------------------------
     # Set up arrays for holding event info to be written to branches
     #---------------------------------------------------------
@@ -62,18 +68,19 @@ def main():
     m_alpha_em = array('f',[0.0])
     m_alpha_s = array('f',[0.0])
 
-    m_pdgid = array('i',[0])
-    m_mother1 = array('i',[0])
-    m_mother2 = array('i',[0])
-    m_color1 = array('i',[0])
-    m_color2 = array('i',[0])
-    m_px = array('f',[0.0])
-    m_py = array('f',[0.0])
-    m_pz = array('f',[0.0])
-    m_e = array('f',[0.0])
-    m_m = array('f',[0.0])
-    m_tau = array('f',[0.0])
-    m_spin = array('f',[0.0])
+    m_pdgid = array('i',[])
+    m_status = array('i',[])
+    m_mother1 = array('i',[])
+    m_mother2 = array('i',[])
+    m_color1 = array('i',[])
+    m_color2 = array('i',[])
+    m_px = array('f',[])
+    m_py = array('f',[])
+    m_pz = array('f',[])
+    m_e = array('f',[])
+    m_m = array('f',[])
+    m_tau = array('f',[])
+    m_spin = array('f',[])
 
     #---------------------------------------------------------
     # Set up TTree and branches for storing info
@@ -113,17 +120,45 @@ def main():
             is_event = True
             is_meta = True
             num_events += 1
+            continue
             #getMetaInfo(line.next().strip().split(' '))
-        if (line.find("</event>") != -1): is_event = False
+        if (line.find("</event>") != -1):
+            is_event = False
+            is_meta = False #Just in case this never got flipped back somehow
+            my_tree.Fill() #Should fill the tree at the end of each event structure!
         if is_event and is_meta:
             getMetaInfo(line.strip().split(' '))
+            m_num_particles[0] = num_particles
+            m_event_weight[0] = event_weight
+            m_event_scale[0] = event_scale
+            m_alpha_em[0] = alpha_em
+            m_alpha_s[0] = alpha_s
             is_meta = False
         if is_event and not is_meta:
             eventInfo = line.strip().split(' ')
-            
+            data = []
+            for n in range(0,len(eventInfo)):
+                if eventInfo[n] != '':
+                    data.append(eventInfo[n])
+            if len(data) != 13: print('Mismatched number of data elements!')
+            else:
+                m_pdgid.append(data[0])
+                m_status.append(data[1])
+                m_mother1.append(data[2])
+                m_mother2.append(data[3])
+                m_color1.append(data[4])
+                m_color2.append(data[5])
+                m_px.append(data[6])
+                m_py.append(data[7])
+                m_pz.append(data[8])
+                m_e.append(data[9])
+                m_m.append(data[10])
+                m_tau.append(data[11])
+                m_spin.append(data[12])
 
     input_file.close()
-    out_file.close()
+    out_file.Write()
+    out_file.Close()
 
 if __name__=="__main__":
     main()
